0435d6dbec67f9760234c8fd813d4ac995c4ee9b
Tyler Geerdts <tygee713@gmail.com>
2022-12-09 14:45:34 -0500

    Added projectiles

--- src/objects/enemy.js
+++ src/objects/enemy.js
@@ -2,28 +2,32 @@ import { init, Sprite } from '../../lib/kontra.min.mjs'
 import Player from '../objects/player.js'
 
 let { canvas } = init()
-initKeys()
 
 const enemyAttributes = {
   'goblin': {
     range: 300,
     speed: 1,
-    projectileSpeed: 10,
+    projectile: 'rock',
+    fireInterval: 3,
   }
 }
 
-const createEnemy = (x, y, type) => Sprite({
-  width: 32,
-  height: 32,
-  x,
-  y,
-  anchor: { x: 0.5, y: 0.5 },
-  range,
-  update: function(dt) {
-    // update which way it's facing depending on player position
-    // move towards the player position if the distance to the player position is greater than its range
-    // otherwise, shoot a projectile towards current player position
-  }
-})
+const createEnemy = (x, y, type) => {
+  const { range, speed, projectile, fireInterval } = enemyAttributes[type]
+  return Sprite({
+    width: 32,
+    height: 32,
+    x,
+    y,
+    anchor: { x: 0.5, y: 0.5 },
+    range,
+    timeSinceAttack: 0,
+    update: function(dt) {
+      // update which way it's facing depending on player position
+      // move towards the player position if the distance to the player position is greater than its range
+      // otherwise, shoot a projectile towards current player position if timeSinceAttack > fireInterval
+    }
+  })
+}
 
 export default createEnemy
---
+++ src/objects/projectile.js
@@ -0,0 +1,40 @@
+import { init, Sprite } from '../../lib/kontra.min.mjs'
+
+let { canvas } = init()
+
+const projectileAttributes = {
+  'rock': {
+    speed: 10,
+    damage: 1,
+    image: null,
+    width: 3,
+    height: 3,
+  }
+}
+
+const createProjectile = (x, y, targetX, targetY, type) => {
+  const { speed, damage, image, width, height } = projectileAttributes[type]
+
+  // Calculate the velocity
+  const angle = Math.atan2(targetY, targetX)
+  const xDelta = Math.cos(angle) * speed
+  const yDelta = Math.sin(angle) * speed
+
+  return Sprite({
+    width,
+    height,
+    x,
+    y,
+    xDelta,
+    yDelta,
+    damage,
+    image,
+    anchor: { x: 0.5, y: 0.5 },
+    update: function(dt) {
+      this.x += this.xDelta
+      this.y += this.yDelta
+    }
+  })
+}
+
+export default createProjectile
