e5977ff3cbacc857fd3deb363b30b658d1904671
Tyler Geerdts <tygee713@gmail.com>
2022-12-15 22:58:05 -0500

    Finished fixing attack animations

--- src/objects/enemy.js
+++ src/objects/enemy.js
@@ -11,7 +11,7 @@ const enemyAttributes = {
     castInterval: 3,
     health: 1,
     castTime: 1,
-    afterCastTime: 1/7,
+    afterCastFrames: 2,
     imagePath: 'assets/goblin.png',
     width: 28,
     height: 34,
@@ -25,14 +25,10 @@ const enemyAttributes = {
         frames: 1,
       },
       westAttack: {
-        frames: [6, 7, 9, 10, 12, 13, 14, 15],
-        frameRate: 7,
+        frames: [6, 7, 9, 10, 9, 10, 9, 10, 9, 10, 12, 13, 14, 15],
+        frameRate: 10,
+        loop: false,
       },
-      // westAfterAttack: {
-      //   frames: 15,
-      //   frameRate: 7,
-      //   
-      // },
       eastWalk: {
         frames: [3, 4, 5, 4],
         frameRate: 10,
@@ -41,154 +37,10 @@ const enemyAttributes = {
         frames: 4,
       },
       eastAttack: {
-        frames: [18, 19, 21, 22, 24, 25, 26, 27],
-        frameRate: 7,
-      },
-      // eastAfterAttack: {
-      //   frames: 27,
-      //   frameRate: 10,
-      //   
-      // },
-    },
-  },
-  'goblincannon': {
-    range: 400,
-    speed: 1,
-    projectileType: 'cannonball',
-    castInterval: 4,
-    health: 5,
-    castTime: 2,
-    afterCastTime: 6/7,
-    imagePath: 'assets/goblincannon.png',
-    width: 48,
-    height: 50,
-    animations: {
-      westWalk: {
-        frames: [0, 1, 2, 1],
+        frames: [18, 19, 21, 22, 21, 22, 21, 22, 21, 22, 24, 25, 26, 27],
         frameRate: 10,
+        loop: false,
       },
-      westIdle: {
-        frames: 1,
-      },
-      westAttack: {
-        frames: [6, 7, 8, 9, 10, 12, 13, 14, 15, 16],
-        frameRate: 7 / 2,
-      },
-      // westAfterAttack: {
-      //   frames: [14, 15, 16],
-      //   frameRate: 10,
-        
-      // },
-      eastWalk: {
-        frames: [3, 4, 5, 4],
-        frameRate: 10,
-      },
-      eastIdle: {
-        frames: 4,
-      },
-      eastAttack: {
-        frames: [18, 19, 20, 21, 22, 24, 25, 26, 27, 28],
-        frameRate: 7 / 2,
-      },
-      // eastAfterAttack: {
-      //   frames: [26, 27, 28],
-      //   frameRate: 10,
-        
-      // },
-    },
-  },
-  'goblinmage': {
-    range: 500,
-    speed: 1.3,
-    projectileType: 'fish',
-    castInterval: 2,
-    health: 3,
-    castTime: 2,
-    afterCastTime: 1,
-    imagePath: 'assets/goblinmage.png',
-    width: 50,
-    height: 52,
-    animations: {
-      westWalk: {
-        frames: [0, 1, 2, 1],
-        frameRate: 10,
-      },
-      westIdle: {
-        frames: 1,
-      },
-      westAttack: {
-        frames: [6, 7, 9, 10, 12, 13, 14, 15, 16],
-        frameRate: 3,
-      },
-      // westAfterAttack: {
-      //   frames: [13, 14, 15, 16],
-      //   frameRate: 10,
-        
-      // },
-      eastWalk: {
-        frames: [3, 4, 5, 4],
-        frameRate: 10,
-      },
-      eastIdle: {
-        frames: 4,
-      },
-      eastAttack: {
-        frames: [18, 19, 21, 22, 24, 25, 26, 27, 28],
-        frameRate: 3,
-      },
-      // eastAfterAttack: {
-      //   frames: [25, 26, 27, 28],
-      //   frameRate: 10,
-        
-      // },
-    },
-  },
-  'floatingeye': {
-    range: 500,
-    speed: 1.6,
-    projectileType: 'darkmatter',
-    castInterval: 2,
-    health: 4,
-    castTime: 1,
-    afterCastTime: 3/6,
-    imagePath: 'assets/floatingeye.png',
-    width: 30,
-    height: 42,
-    animations: {
-      westWalk: {
-        frames: [0, 1, 2, 1],
-        frameRate: 10,
-      },
-      westIdle: {
-        frames: 1,
-      },
-      westAttack: {
-        frames: [6, 7, 9, 10, 12, 13, 14, 15, 16],
-        frameRate: 6,
-        
-      },
-      // westAfterAttack: {
-      //   frames: [14, 15, 16],
-      //   frameRate: 10,
-        
-      // },
-      eastWalk: {
-        frames: [3, 4, 5, 4],
-        frameRate: 10,
-      },
-      eastIdle: {
-        frames: 4,
-      },
-      eastAttack: {
-        frames: [18, 19, 21, 22, 24, 25, 26, 27, 28],
-        frameRate: 6,
-        
-      },
-      // eastAfterAttack: {
-      //   frames: [26, 27, 28],
-      //   frameRate: 10,
-        
-      // },
     },
   },
   'archer': {
@@ -198,7 +50,7 @@ const enemyAttributes = {
     castInterval: 1,
     health: 2,
     castTime: 1,
-    afterCastTime: 3/5,
+    afterCastFrames: 2,
     imagePath: 'assets/archer.png',
     width: 42,
     height: 40,
@@ -211,13 +63,10 @@ const enemyAttributes = {
         frames: 1,
       },
       westAttack: {
-        frames: [6, 7, 9, 10, 12, 13, 15, 16],
-        frameRate: 5,
+        frames: [6, 7, 9, 10, 9, 10, 9, 10, 9, 10, 12, 13, 15, 16],
+        frameRate: 10,
+        loop: false,
       },
-      // westAfterAttack: {
-      //   frames: [13, 15, 16],
-      //   frameRate: 10,
-      // },
       eastWalk: {
         frames: [3, 4, 5, 4],
         frameRate: 10,
@@ -226,15 +75,124 @@ const enemyAttributes = {
         frames: 4,
       },
       eastAttack: {
-        frames: [18, 19, 21, 22, 24, 25, 27, 28],
-        frameRate: 5,
-        
+        frames: [18, 19, 21, 22, 21, 22, 21, 22, 21, 22, 24, 25, 27, 28],
+        frameRate: 10,
+        loop: false,
       },
-      // eastAfterAttack: {
-      //   frames: [25, 27, 28],
-      //   frameRate: 10,
-        
-      // },
+    },
+  },
+  'goblincannon': {
+    range: 400,
+    speed: 1,
+    projectileType: 'cannonball',
+    castInterval: 4,
+    health: 5,
+    castTime: 1.9,
+    afterCastFrames: 3,
+    imagePath: 'assets/goblincannon.png',
+    width: 48,
+    height: 50,
+    animations: {
+      westWalk: {
+        frames: [0, 1, 2, 1],
+        frameRate: 10,
+      },
+      westIdle: {
+        frames: 1,
+      },
+      westAttack: {
+        frames: [6, 7, 8, 9, 10, 9, 10, 9, 10, 9, 10, 9, 10, 9, 10, 9, 10, 9, 10, 12, 13, 14, 15, 16],
+        frameRate: 10,
+        loop: false,
+      },
+      eastWalk: {
+        frames: [3, 4, 5, 4],
+        frameRate: 10,
+      },
+      eastIdle: {
+        frames: 4,
+      },
+      eastAttack: {
+        frames: [18, 19, 20, 21, 22, 21, 22, 21, 22, 21, 22, 21, 22, 21, 22, 21, 22, 21, 22, 24, 25, 26, 27, 28],
+        frameRate: 10,
+        loop: false,
+      },
+    },
+  },
+  'goblinmage': {
+    range: 500,
+    speed: 1.3,
+    projectileType: 'fish',
+    castInterval: 2,
+    health: 3,
+    castTime: 1.6,
+    afterCastFrames: 3,
+    imagePath: 'assets/goblinmage.png',
+    width: 50,
+    height: 52,
+    animations: {
+      westWalk: {
+        frames: [0, 1, 2, 1],
+        frameRate: 10,
+      },
+      westIdle: {
+        frames: 1,
+      },
+      westAttack: {
+        frames: [6, 7, 9, 10, 9, 10, 9, 10, 9, 10, 9, 10, 9, 10, 9, 10, 12, 13, 14, 15, 16],
+        frameRate: 10,
+        loop: false,
+      },
+      eastWalk: {
+        frames: [3, 4, 5, 4],
+        frameRate: 10,
+      },
+      eastIdle: {
+        frames: 4,
+      },
+      eastAttack: {
+        frames: [18, 19, 21, 22, 21, 22, 21, 22, 21, 22, 21, 22, 21, 22, 21, 22, 24, 25, 26, 27, 28],
+        frameRate: 10,
+        loop: false,
+      },
+    },
+  },
+  'floatingeye': {
+    range: 500,
+    speed: 1.6,
+    projectileType: 'darkmatter',
+    castInterval: 2,
+    health: 4,
+    castTime: 1.2,
+    afterCastFrames: 3,
+    imagePath: 'assets/floatingeye.png',
+    width: 30,
+    height: 42,
+    animations: {
+      westWalk: {
+        frames: [0, 1, 2, 1],
+        frameRate: 10,
+      },
+      westIdle: {
+        frames: 1,
+      },
+      westAttack: {
+        frames: [6, 7, 9, 10, 9, 10, 9, 10, 9, 10, 9, 10, 12, 13, 14, 15, 16],
+        frameRate: 10,
+        loop: false,
+      },
+      eastWalk: {
+        frames: [3, 4, 5, 4],
+        frameRate: 10,
+      },
+      eastIdle: {
+        frames: 4,
+      },
+      eastAttack: {
+        frames: [18, 19, 21, 22, 21, 22, 21, 22, 21, 22, 21, 22, 24, 25, 26, 27, 28],
+        frameRate: 10,
+        loop: false,
+      },
     },
   },
   'skeleton': {
@@ -243,8 +201,8 @@ const enemyAttributes = {
     projectileType: 'bone',
     castInterval: 0.5,
     health: 1,
-    castTime: 0.5,
-    afterCastTime: 0.2,
+    castTime: 0.6,
+    afterCastFrames: 2,
     imagePath: 'assets/skeleton.png',
     width: 28,
     height: 34,
@@ -258,15 +216,10 @@ const enemyAttributes = {
         frames: 1,
       },
       westAttack: {
-        frames: [6, 7, 9, 10, 12, 13, 15],
+        frames: [6, 7, 9, 10, 9, 10, 12, 13, 15],
         frameRate: 10,
-        
+        loop: false,
       },
-      // westAfterAttack: {
-      //   frames: [13, 15],
-      //   frameRate: 10,
-        
-      // },
       eastWalk: {
         frames: [3, 4, 5, 4],
         frameRate: 10,
@@ -275,15 +228,10 @@ const enemyAttributes = {
         frames: 4,
       },
       eastAttack: {
-        frames: [18, 19, 21, 22, 24, 25, 27],
+        frames: [18, 19, 21, 22, 21, 22, 24, 25, 27],
         frameRate: 10,
-        
+        loop: false,
       },
-      // eastAfterAttack: {
-      //   frames: [25, 27],
-      //   frameRate: 10,
-        
-      // },
     },
   },
 }
@@ -299,7 +247,7 @@ const createCollider = () => Sprite({
 })
 
 const createEnemy = (x, y, type, scene) => {
-  const { range, speed, projectileType, castInterval, health, castTime, afterCastTime, imagePath, width, height, minRange, animations } = enemyAttributes[type]
+  const { range, speed, projectileType, castInterval, health, afterCastFrames, imagePath, width, height, minRange, animations } = enemyAttributes[type]
   const collider = createCollider()
   let enemy = Sprite({
     width,
@@ -310,12 +258,10 @@ const createEnemy = (x, y, type, scene) => {
     range,
     speed,
     minRange: minRange || defaultMinRange,
-    timeSinceAttack: 0,
-    attackAnimationTime: 0,
-    castTime,
-    afterCastTime,
+    timeSinceLastAttack: 0,
+    attacking: false,
+    afterCastFrames,
     health,
-    facing: null,
     scene,
     castInterval,
     hitByProjectiles: [],
@@ -329,7 +275,7 @@ const createEnemy = (x, y, type, scene) => {
         return
       }
 
-      this.timeSinceAttack += dt
+      this.timeSinceLastAttack += dt
       this.currentAnimation && this.currentAnimation.update(dt)
 
       // update which way it's facing depending on player position
@@ -343,7 +289,7 @@ const createEnemy = (x, y, type, scene) => {
       let yDistance = this.y - Player.y
       let distanceToPlayer = Math.sqrt(xDistance * xDistance + yDistance * yDistance)
       // if the enemy is too close to the player, do not move
-      if ((distanceToPlayer > this.range || (distanceToPlayer > this.minRange && this.timeSinceAttack < this.castInterval)) && !this.attackAnimationTime) {
+      if ((distanceToPlayer > this.range || (distanceToPlayer > this.minRange && this.timeSinceLastAttack < this.castInterval)) && !this.attacking) {
         // move towards the player position if the distance to the player position is greater than its range
         let xDiff = Player.x - this.x
         let yDiff = Player.y - this.y
@@ -353,7 +299,8 @@ const createEnemy = (x, y, type, scene) => {
         this.playAnimation(this.facing + 'Walk')
       } else {
         // otherwise, start attack towards current player position if their cooldown is up
-        if (this.timeSinceAttack >= this.castInterval) {
+        if (this.timeSinceLastAttack >= this.castInterval) {
+          this.attacking = true
           // set initial direction the attack is facing, in case enemy switches their direction during
           if (!this.attackingDirection) {
             this.attackingDirection = this.facing
@@ -366,19 +313,19 @@ const createEnemy = (x, y, type, scene) => {
           if (this.facing !== this.attackingDirection) {
             this.currentAnimation.frames = [...this.animations[this.facing + 'Attack'].frames]
           }
-          
-          this.attackAnimationTime += dt
-          // once the animation time finishes, the character shoots a projectile
-          if (this.attackAnimationTime >= this.castTime) {
+
+          let lastAnimationFrame = this.currentAnimation.frames.length - 1
+
+          if (this.currentAnimation._f >= lastAnimationFrame - this.afterCastFrames) {
             if (!this.attacked) {
               let xOffset = width / 2
               if (this.facing === 'west') xOffset = xOffset * -1
               this.attacked = true
               this.scene.shootProjectile(this.x + xOffset, this.y, projectileType, this)
-            } else if (this.attackAnimationTime > this.castTime + this.afterCastTime) {
-              this.timeSinceAttack = 0
-              this.attackAnimationTime = 0
+            } else if (this.currentAnimation._f == lastAnimationFrame) {
+              this.timeSinceLastAttack = 0
               this.attacked = false
+              this.attacking = false
               this.attackingDirection = null
             }
           }
